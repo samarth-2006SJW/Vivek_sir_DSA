c68ca6c4a564f9c2f8c4ba106ebb27c2ae9a250c
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-11-10 21:02:28 +0530

    deletion

---
+++ singly_linked_list/delete_last_node.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+     int data;
+     Node *next;
+};
+Node *first=NULL,*temp=NULL,*ttemp=NULL;
+void add_node(int x,Node* &first)
+{
+    if(first==NULL)
+    {
+        first=new Node();
+        first->data=x;
+        first->next=NULL;
+    }
+    else
+    {
+    temp=first;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    Node* p=new Node();
+    p->data=x;
+    p->next=NULL;
+    temp->next=p;
+    }
+}
+void delete_last_node(Node* &first)
+{
+    temp=first;
+    while(temp->next!=NULL)
+    {
+     ttemp=temp;
+     temp=temp->next;
+    } 
+    ttemp->next=NULL;
+    delete temp;
+}
+void display( Node* &first)
+{
+    temp=first;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+        
+    }
+    cout<<endl;
+}
+int main()
+{
+    add_node(1,first);
+    add_node(2,first);
+    add_node(3,first);
+    add_node(4,first);
+    add_node(5,first);
+    cout<<"Linked list:";
+    display(first);
+    delete_last_node(first);
+    cout<<"Linked list after deletion:";
+    display(first);
+    return 0;
+
+
+}
+
