dc41e9e50c8333967f94861e90ad68b2af4ccd8f
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-12-02 12:00:00 +0530

    addition

---
+++ doubly_linked_list/add_node_before_data.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    Node *prev;
+    int data;
+    Node *next;
+};
+Node *first=NULL,*temp,*ttemp;
+void add_node(int x,Node* &first)
+{
+    if(first==NULL)
+    {
+        first=new Node();
+        first->data=x;
+        first->next=NULL;
+        first->prev=NULL;
+    }
+    else
+    {
+        temp=first;
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        ttemp=new Node();
+        ttemp->data=x;
+        ttemp->next=NULL;
+        ttemp->prev=temp;
+        temp->next=ttemp;
+    }
+}
+void add_before_data(int x,int y,Node* &first)
+{
+    temp=first;
+    while(temp->data!=x)
+    {
+        temp=temp->next;
+    }
+    Node *p=new Node();
+    p->data=y;
+    p->next=temp;
+    temp->prev->next=p;
+    p->prev=temp->prev;
+    temp->prev=p;
+}
+void display(Node* &first)
+{
+    temp=first;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    add_node(5,first);
+    add_node(6,first);
+    add_node(7,first);
+    add_node(8,first);
+    add_node(9,first);
+    add_node(10,first);
+    cout<<"Doubly Linked List:";
+    display(first);
+    add_before_data(7,11,first);
+    cout<<"Doubly Linked List:";
+    display(first);
+    return 0;
+}
