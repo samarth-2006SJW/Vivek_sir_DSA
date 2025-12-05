921269a60f5e7adf0141f46060d578497dbab5cf
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-12-05 12:00:00 +0530

    Swapping

---
+++ doubly_linked_list/swap_SL_L.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    Node *prev;
+    int data;
+    Node *next;
+};
+Node *first=NULL,*temp,*ttemp;
+void add_node(int x,Node* &first)
+{
+    if(first==NULL)
+    {
+        first=new Node();
+        first->data=x;
+        first->next=NULL;
+        first->prev=NULL;
+    }
+    else{
+        temp=first;
+        while(temp->next!=NULL)
+         temp=temp->next;
+        ttemp=new Node();
+        ttemp->data=x;
+        ttemp->next=NULL;
+        ttemp->prev=temp;
+        temp->next=ttemp; 
+    }
+}    
+void swap_SL_L(Node* &first)
+{
+       temp=first;
+       while(temp->next!=NULL)
+       {
+        ttemp=temp;
+        temp=temp->next;
+       }
+       Node* p=first;
+       while(p->next!=ttemp)
+        p=p->next;
+       temp->next=ttemp;
+       temp->prev=p;
+       p->next=temp;
+       ttemp->prev=temp;
+       ttemp->next=NULL;
+        
+}
+    void display(Node* &first)
+{
+    temp=first;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    add_node(5,first);
+    add_node(6,first);
+    add_node(7,first);
+    add_node(8,first);
+    add_node(9,first);
+    add_node(10,first);
+    cout<<"Doubly Linked List:";
+    display(first);
+    swap_SL_L(first);
+    cout<<"Doubly Linked List:";
+    display(first);
+    return 0;
+}
