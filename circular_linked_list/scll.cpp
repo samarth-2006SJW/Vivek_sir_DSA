c88db342e8624c28890ee51b77f2f06f90b4ebc9
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-12-21 22:06:02 +0530

    Implementation

---
+++ circular_linked_list/scll.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node *next;
+};
+Node *first=NULL,*temp,*ttemp;
+void add_element(int x,Node* &first)
+{
+    if(first==NULL)
+    {
+        first=new Node();
+        first->data=x;
+        first->next=first;
+    }
+    else{
+    temp=first;
+    while(temp->next!=first)
+     temp=temp->next;
+    ttemp=new Node();
+    ttemp->data=x;
+    ttemp->next=first;
+    temp->next=ttemp;
+    } 
+}
+void display(Node* first)
+{
+    temp=first;
+    do
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    } while(temp!=first);
+    cout<<endl;
+}
+int main()
+{
+   
+    add_element(7,first);
+    add_element(8,first);
+    add_element(9,first);
+    add_element(10,first);
+    add_element(11,first);
+    add_element(12,first);
+
+    cout<<"Singly Circular Linked List:";
+    display(first);
+    return 0;
+
+}
