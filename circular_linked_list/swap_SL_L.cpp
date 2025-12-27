8fbdb4280d66dc8cbf73f51dffcb3ae065c4e5dc
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-12-27 23:47:20 +0530

    Swapping

---
+++ circular_linked_list/swap_SL_L.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node *next;
+};
+Node *first=NULL,*temp,*ttemp;
+void add_element(int x,Node* &first)
+{
+    if(first==NULL)
+    {
+        first=new Node();
+        first->data=x;
+        first->next=first;
+    }
+    else{
+    temp=first;
+    while(temp->next!=first)
+     temp=temp->next;
+    ttemp=new Node();
+    ttemp->data=x;
+    ttemp->next=first;
+    temp->next=ttemp;
+    } 
+}
+void swap_SL_L(Node* &first)
+{
+   temp=first;
+   while(temp->next!=first)
+    {
+        ttemp=temp;
+        temp=temp->next;
+    }
+   Node* p=first;
+   while(p->next!=ttemp)
+   {
+     p=p->next;
+   } 
+   p->next=temp;
+   ttemp->next=first;
+   temp->next=ttemp;
+}
+void display(Node* first)
+{
+    temp=first;
+    do
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    } while(temp!=first);
+    cout<<endl;
+}
+int main()
+{
+   
+    add_element(7,first);
+    add_element(8,first);
+    add_element(9,first);
+    add_element(10,first);
+    // add_element(11,first);
+    // add_element(12,first);
+
+    cout<<"Singly Circular Linked List:";
+    display(first);
+    swap_SL_L(first);
+    cout<<"Singly Circular Linked list after Swapping:";
+    display(first);
+    return 0;
+
+}
