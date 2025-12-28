1b65597a8e3c79a9c17027dda42774acfa383dc6
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-12-28 23:41:05 +0530

    Deletion

---
+++ doubly_circular_linked__list/delete_after_node.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    Node *prev;
+    int data;
+    Node *next;
+};
+Node *first=NULL,*temp,*ttemp;
+void add_element(int x,Node* &first)
+{
+    if(first==NULL)
+    {
+        first=new Node();
+        first->data=x;
+        first->next=first;
+        first->prev=first;
+    }
+    else{
+        temp=first->prev;
+        ttemp=new Node();
+        ttemp->data=x;
+        ttemp->next=first;
+        ttemp->prev=temp;
+        temp->next=ttemp;
+        first->prev=ttemp;  
+    }
+}
+void delete_after_node(int x,Node* &first)
+{
+    temp=first;
+    while(temp->data!=x)
+     temp=temp->next;
+    ttemp=temp->next;
+    temp->next=temp->next->next;
+    temp->next->next->prev=temp;
+    ttemp->prev=NULL;
+    ttemp->next=NULL;
+    delete ttemp;
+
+}
+void display(Node* &first)
+{
+    temp=first;
+    do{
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }while(temp!=first);
+    cout<<endl;
+}
+int main()
+{
+    add_element(6,first);
+    add_element(7,first);
+    add_element(8,first);
+    add_element(9,first);
+    add_element(10,first);
+    cout<<"Doubly Circular Linked List:";
+    display(first);
+    delete_after_node(7,first);
+    cout<<"Doubly Circular Linked List after deletion:";
+    display(first);
+    return 0; 
+   
+
+}
