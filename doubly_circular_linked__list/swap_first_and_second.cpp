8e36a9caaab391542993095ca7fc7adb4315d498
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-12-31 23:57:20 +0530

    Swapping

---
+++ doubly_circular_linked__list/swap_first_and_second.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    Node *prev;
+    int data;
+    Node *next;
+};
+Node *first=NULL,*temp,*ttemp;
+void add_element(int x,Node* &first)
+{
+    if(first==NULL)
+    {
+        first=new Node();
+        first->data=x;
+        first->next=first;
+        first->prev=first;
+    }
+    else{
+        temp=first->prev;
+        ttemp=new Node();
+        ttemp->data=x;
+        ttemp->next=first;
+        ttemp->prev=temp;
+        temp->next=ttemp;
+        first->prev=ttemp;  
+    }
+}
+void swap_F_S(Node* &first)
+{
+    temp=first->prev;
+    ttemp=first->next;
+    first->next=ttemp->next;
+    ttemp->next->prev=first;
+    first->prev=ttemp;
+    ttemp->next=first;
+    ttemp->prev=temp;
+    temp->next=ttemp;
+    first=ttemp;
+}
+void display(Node* &first)
+{
+    temp=first;
+    do{
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }while(temp!=first);
+    cout<<endl;
+}
+int main()
+{
+    add_element(6,first);
+    add_element(7,first);
+    add_element(8,first);
+    add_element(9,first);
+    add_element(10,first);
+    cout<<"Doubly Circular Linked List:";
+    display(first);
+    swap_F_S(first);
+    cout<<"Doubly Circular Linked List after Swapping:";
+    display(first);
+    return 0; 
+   
+
+}
